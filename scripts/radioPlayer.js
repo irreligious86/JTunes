70484980ec78b3b90442372b9a88d069a7ecd7c4
Aleksander Voronov <irreligious86@gmail.com>
2020-12-23 16:28:11 +0200

    my first radio

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,3 +1,33 @@
 export const radioPlayerInit = () => {
     console.log('radio Player');
+
+    const radio = document.querySelector('.radio');
+    const radioCover__img = document.querySelector('.radio-cover');
+    const radioHeader = document.querySelector('.radio-header');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disabled = true;
+
+    radioNavigation.addEventListener('change', (evt) => {
+        radioStop.disabled = false;
+        const target = evt.target;
+        audio.src = target.dataset.radioStantion;
+        console.log(audio.src);
+        audio.play();
+    });
+
+    radioStop.addEventListener('click', () => {
+        if(audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+    });
+
+
 };
--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -122,6 +122,4 @@ videoVolUpBtn.addEventListener('dblclick', () => {
         changeValue();
 });
 
-
-
 };
